e2268923fba7b56747013c775ad3c4e06efbce7f
Bruno51-G <185206289+Bruno51-G@users.noreply.github.com>
2026-02-24 16:49:20 +0100

    suite de l'exo

--- wp_app/wp-content/themes/monTheme/footer.php
+++ wp_app/wp-content/themes/monTheme/footer.php
@@ -5,10 +5,9 @@
     Copyright Bruno GODBILLOT 2026
 </footer>
 
-<?php wp_footer(); ?> // permet d'insérer du code juste avant la fermeture de la balise </body>, utile pour les plugins ou les thèmes enfants
+<?php wp_footer(); ?> <!-- permet d'insérer du code juste avant la fermeture de la balise </body>, utile pour les plugins ou les thèmes enfants -->
 
 </body>
 </html>
 
 <!-- FIN DU FOOTER -->
- 
