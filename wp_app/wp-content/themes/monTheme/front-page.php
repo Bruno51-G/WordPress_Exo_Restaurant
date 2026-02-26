c1b398ac2670d864049af3bcb2c4ab70a23348bf
Bruno51-G <185206289+Bruno51-G@users.noreply.github.com>
2026-02-26 16:55:33 +0100

    modif

--- wp_app/wp-content/themes/monTheme/front-page.php
+++ wp_app/wp-content/themes/monTheme/front-page.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-<h1>INDEX.PHP</h1>
+<h1>FRONT-PAGE.PHP</h1>
 <div class="montheme-articles-grid">
 <?php 
     if(have_posts()): // si l'url appelé correspond à du contenu  (article, page, auteur, catégorie...)
@@ -24,6 +24,11 @@ get_header();
     endif;
 ?>
 </div>
+
+<aside>
+    <h3>SIDEBAR</h3>
+    <h4>Widgets</h4>
+</aside>
 
 <?php 
 get_footer();
--- wp_app/wp-content/themes/monTheme/home.php
+++ wp_app/wp-content/themes/monTheme/home.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-<h1>INDEX.PHP</h1>
+<h1>HOME.PHP</h1>
 <div class="montheme-articles-grid">
 <?php 
     if(have_posts()): // si l'url appelé correspond à du contenu  (article, page, auteur, catégorie...)
